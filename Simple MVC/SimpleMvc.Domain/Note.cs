050d85f15943e48879afb20a462fafeeb84e653c
Dilyana <dilyana_stz@abv.bg>
2018-08-11 09:46:59 +0300

    Simple MVC

---
+++ Simple MVC/SimpleMvc.Domain/Note.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleMvc.Domain
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int OwnerId { get; set; }
+
+        [Required]
+        public User Owner { get; set; }
+    }
+}
---
+++ Simple MVC/SimpleMvc.Framework/Contracts/IActionResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleMvc.Framework.Contracts
+{
+    public interface IActionResult : IInvocable
+    {
+        IRenderable Action { get; set; }
+    }
+}
